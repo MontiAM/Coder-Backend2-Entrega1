c6ced17a93215d7c5e177b2c93df7e0d51ca1782
Marcio Montivero <montivero.marcio@gmail.com>
2025-09-06 13:09:36 -0300

    limpiar de handlebars

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -1,5 +1,4 @@
 // Routes
-import viewsRouter from "./vews/views.route.js";
 import productsRoute from "./api/products.routes.js";
 import cartRoute from "./api/cart.routes.js";
 import userRouter from "./api/users.route.js";
@@ -10,7 +9,6 @@ import { passportCall } from "../middlewares/passport/passport-call.js";
 
 class IndexRoute {
     initRoutes(app) {
-        app.use("/", viewsRouter);
         app.use(
             "/api/products",
             passportCall("jwtCookies"),
@@ -24,7 +22,7 @@ class IndexRoute {
             cartRoute
         );
         app.use("/api/users", userRouter);
-        app.use("/api/sessions", sessionRouter);
+        app.use("/api/sessions", passportCall("jwtCookies"), sessionRouter);
     }
 }
 
